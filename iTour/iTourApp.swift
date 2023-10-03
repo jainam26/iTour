8710a0698d1421ed0e19d12e1947405ed48ec23e
Jainam Shah <shah@applescholsy.com>
2023-10-03 16:06:35 -0700

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Jainam  Shah  on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
